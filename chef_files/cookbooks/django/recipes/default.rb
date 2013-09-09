59960ef7165e955382a2d466f868b2b5fc70281b
rogueleaderr <4.business.email@gmail.com>
2013-09-09 15:31:57 -0700

    local chef config changes

--- chef_files/cookbooks/django/recipes/default.rb
+++ chef_files/cookbooks/django/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: django
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, George London
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -23,3 +23,46 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+
+#code derived in part from http://ericholscher.com/blog/2010/nov/9/building-django-app-server-chef-part-2/
+
+node[:base_packages].each do |pkg|
+    package pkg do
+        :upgrade
+    end
+end
+
+node[:users].each_pair do |username, info|
+    group username do
+       gid info[:id]
+    end
+
+    user username do
+        comment info[:full_name]
+        uid info[:id]
+        gid info[:id]
+        shell info[:disabled] ? "/sbin/nologin" : "/bin/bash"
+        supports :manage_home => true
+        home "/home/#{username}"
+    end
+
+    directory "/home/#{username}/.ssh" do
+        owner username
+        group username
+        mode 0700
+    end
+
+    file "/home/#{username}/.ssh/authorized_keys" do
+        owner username
+        group username
+        mode 0600
+        content info[:key]
+    end
+end
+
+node[:groups].each_pair do |name, info|
+    group name do
+        gid info[:gid]
+        members info[:members]
+    end
+end
